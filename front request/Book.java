8214824c8b01539c1f4fd28fa133d96784ca7902
litian <496499417@qq.com>
2018-09-30 12:31:30 +0800

    Create Book.java

---
+++ front request/Book.java
@@ -0,0 +1,107 @@
+package javastudy1;
+
+public class Book {
+	int id;
+	public String shopname;
+	public String qzpd;
+	public String tastegood;
+	public String provence;
+	public String city;
+	public String address;
+	public String contact;
+	public String introduce;
+	public String alipay;
+	public String ok_address;
+	public String longitude;
+	public String latitude;
+	public String general_addr;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getShopname() {
+		return shopname;
+	}
+	public void setShopname(String shopname) {
+		this.shopname = shopname;
+	}
+	public String getQzpd() {
+		return qzpd;
+	}
+	public void setQzpd(String qzpd) {
+		this.qzpd = qzpd;
+	}
+	public String getTastegood() {
+		return tastegood;
+	}
+	public void setTastegood(String tastegood) {
+		this.tastegood = tastegood;
+	}
+	public String getProvence() {
+		return provence;
+	}
+	public void setProvence(String provence) {
+		this.provence = provence;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	public String getAlipay() {
+		return alipay;
+	}
+	public void setAlipay(String alipay) {
+		this.alipay = alipay;
+	}
+	public String getOk_address() {
+		return ok_address;
+	}
+	public void setOk_address(String ok_address) {
+		this.ok_address = ok_address;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getGeneral_addr() {
+		return general_addr;
+	}
+	public void setGeneral_addr(String general_addr) {
+		this.general_addr = general_addr;
+	}
+	
+	
+}
